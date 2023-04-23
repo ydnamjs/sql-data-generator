c09afb4171325278c84a8017b51fbd65ec03d1f1
ydnamjs <ydnamjs@udel.edu>
2023-04-23 02:23:27 -0400

    Implemented banks that are chose from at random instead of constant strings

--- dataMaker.py
+++ dataMaker.py
@@ -1,9 +1,13 @@
+from random import randrange
+
 def main():
     f = open("data.txt", "w")
 
     #//, 'test_title', 'test_first', 'test_last', TO_DATE('2023-04-19 18:36:49', 'YYYY-MM-DD HH24:MI:SS'))
 
-    first_bank = ["bert", "fred", "Raphael", "Piplup", "Ash", "Angelina", "Trinity", ]
+    title_bank = ["King", "Queen", "Manager", "CEO", "Founder", "Salesman", "Fashion-designer", "Teacher", "Pirate", "Cowboy"]
+    first_bank = ["bert", "Fred", "Sean", "Piplup", "Ash", "Angelina", "Trinity", "Blair", "Chris", "Gabriella"]
+    last_bank = ["smith", "steel", "Gonzales", "Brown", "Bart", "Silber", "Roosen", "Felker", "O'Sullivan", "Sherman"]
 
     # constants
     prefix = "INSERT INTO \"UD_437_11_8\".\"EMPLOYMENT_UNINDEXED\" (EMPLOYMENT_ID, JOB_TITLE, EMPLOYEE_FIRST, EMPLOYEE_LAST, HIRE_DATE) VALUES ('"
@@ -13,13 +17,13 @@ def main():
 
     counter = 1;
 
-    while(counter < 10000):
+    while(counter < 25000):
 
         # data field variables
         id = str(counter)
-        title = "Founder"
-        first = "Burt"
-        last = "Gibbons"
+        title = title_bank[randrange(0, 9)]
+        first = first_bank[randrange(0, 9)]
+        last = last_bank[randrange(0, 9)]
         date = '2023-04-19 18:36:49'
 
         built_string = prefix + id + vS + title + vS + first + vS + last + datePref + date + dateSuf
